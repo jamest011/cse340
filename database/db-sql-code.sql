00b0b546983cca1bdff1528a7afe92e5471e6c4a
Jamest011 <ttysjr@gmail.com>
2024-01-20 22:37:44 -0600

    updated owner

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340dbv4;
+    OWNER TO cse340dbv2;
 
 -- Table structure for table 'classification'
 CREATE TABLE public.classification (
