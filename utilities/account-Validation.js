95b01a811ed4430cc1072c71c3fa8ded50b55aca
Jamest011 <ttysjr@gmail.com>
2024-02-02 13:36:52 -0600

    added password hashing, fixed errors, and completed team activity.

--- utilities/account-Validation.js
+++ utilities/account-Validation.js
@@ -26,7 +26,13 @@ validate.registrationRules = () => {
       .trim()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+          throw new Error("Email exists. Please log in or use a different email")
+        }
+      }),
   
       // password is required and must be strong password
       body("account_password")
@@ -62,32 +68,7 @@ validate.checkRegData = async (req, res, next) => {
       return
     }
     next()
-  }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-// // valid email is required and cannot already exist in the database
-// body("account_email")
-//   .trim()
-//   .isEmail()
-//   .normalizeEmail() // refer to validator.js docs
-//   .withMessage("A valid email is required.")
-//   .custom(async (account_email) => {
-//     const emailExists = await accountModel.checkExistingEmail(account_email)
-//     if (emailExists){
-//       throw new Error("Email exists. Please log in or use different email")
-//     }
-//   }),
+}
 
 
 module.exports = validate
